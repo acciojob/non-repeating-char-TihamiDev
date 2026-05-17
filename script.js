3598c4042c4871209f34731648966fb29218398b
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-05-18 01:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	 for (let i = 0; i < str.length; i++) {
+	 	let currentCharacter = str[i]
+		 let count = 0;
+		 for(let j = 0; j < str.length; j++){
+			 if (currentCharacter == str[j]) {
+			 	count ++;
+			 } 
+			 if(count == 1){
+				 return currentCharacter;
+			 }
+		 }
+	 }
+	return null;
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
